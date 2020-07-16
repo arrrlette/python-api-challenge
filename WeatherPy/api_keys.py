60445cd83f253e111df773e99753b8355e36865f
Arlette Varela <65184164+arrrlette@users.noreply.github.com>
2020-07-16 18:33:10 -0400

    final upload

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "cf3374466ad8177d2f8b2f8d9a814984"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyCMS0VJQNS_xazPwErRkzG4pCxdq0_9P8I"
+g_key = ""
