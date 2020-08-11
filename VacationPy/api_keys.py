4c9e8e6ab9222d392c355bca9d226d7b6032f8e9
Arlette Varela <65184164+arrrlette@users.noreply.github.com>
2020-08-11 19:27:32 -0400

    initial push

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "cf3374466ad8177d2f8b2f8d9a814984"
 
 # Google API Key
-g_key = ""
+g_key = "AIzaSyCMS0VJQNS_xazPwErRkzG4pCxdq0_9P8I"
